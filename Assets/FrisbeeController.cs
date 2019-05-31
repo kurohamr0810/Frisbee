8acba53aeee0fb6819fc83f2fb81c06c7cacb4c7
kurohamr0810 <kurohamr@gmal.com>
2019-06-01 02:26:58 +0900

    Add FrisbeeController

---
+++ Assets/FrisbeeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrisbeeController : MonoBehaviour
+{
+    [SerializeField] float risePower = 50f;
+    [SerializeField] float rotatePower= 80f;
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        RiseObjInput();
+        RotateObjInput();
+    }
+
+    private void RiseObjInput()
+    {
+        if(Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * risePower);
+        }
+    }
+
+    private void RotateObjInput()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward * rotatePower * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward * rotatePower * Time.deltaTime);
+        }
+
+    }
+}
