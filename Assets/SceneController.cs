d0dd7eee4b3196d90f9c3f214ac7a7de3dbf36cc
kurohamr0810 <kurohamr@gmal.com>
2019-06-02 01:31:06 +0900

    Add Scene Index

---
+++ Assets/SceneController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            int currentScenelndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentScenelndex + 1;
+
+
+            if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            {
+                nextSceneIndex = 0;
+            }
+
+            SceneManager.LoadScene(nextSceneIndex);
+
+        }
+
+
+    }
+}
