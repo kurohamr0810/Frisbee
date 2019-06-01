2e5bfb0683f32b3e05f6e67f00802b12f61268ad
kurohamr0810 <kurohamr@gmal.com>
2019-06-01 21:23:39 +0900

    Add CameraController

---
+++ Assets/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        Vector3 playerPos = this.player.transform.position;
+        transform.position = new Vector3(playerPos.x, transform.position.y, transform.position.z);
+    }
+}
